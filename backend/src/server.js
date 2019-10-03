edf1b7edd31eb3491dc96938f92b640a8d93cedb
Bruno Ribeiro <brunomestres@gmail.com>
2019-10-03 14:19:47 -0300

    Conexão ok

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,13 +1,14 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const routes = require('./routes');
-
+const cors = require('cors');
 const app = express();
 
 mongoose.connect('mongodb://omnistack:omnistack@cluster0-shard-00-00-ys386.mongodb.net:27017,cluster0-shard-00-01-ys386.mongodb.net:27017,cluster0-shard-00-02-ys386.mongodb.net:27017/aircnc?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin&retryWrites=true&w=majority',{
     useNewUrlParser:true,
     useUnifiedTopology:true  
-})
+});
+app.use(cors());
 
 app.use(express.json());
 
